b15df7c69b457b35465bc75b82252dca819487b3
luisserranom <smirandaluis1999gmail.com>
2024-12-20 00:39:59 -0300

    se agregan cambios de estilos y cambio de logica de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import ContainerCard from './components/ContainerCard/ContainerCard';
-import Footer from './components/Footer/Footer';
-import './main.scss';
+import Home from './screens/Home';
 
 
 function App() {
   return (
     <>
-      <ContainerCard />
-      <Footer />
-     
+      <Home />
     </>
   );
 }
---
+++ src/screens/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ContainerCard from '../components/ContainerCard/ContainerCard'
+import Footer from '../components/Footer/Footer'
+function Home() {
+  return (
+    <>
+        {/* Aqui agregar navbar */}
+        <ContainerCard />
+        <Footer />
+    </>
+    
+  )
+}
+
+export default Home
